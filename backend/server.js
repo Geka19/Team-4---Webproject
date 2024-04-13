11f34710d61665015d9389c7ead5ac56ea3159ec
simen <111754546+simeel@users.noreply.github.com>
2024-04-13 13:31:38 +0200

    added some routes for user

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ dotenv.config(); // load env variables
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const connectDB = require("./connectDB");
-//const ejs = require("ejs");
+// const ejs = require("ejs");
 
 // initiate express app
 const app = express();
@@ -13,11 +13,13 @@ const app = express();
 // connectDB();
 
 // config express app
-//app.use('/', express.static("public"));
+// app.use('/', express.static("public"));
 
 app.use("/", (req, res) => {
   res.end("hello there");
 });
+
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
