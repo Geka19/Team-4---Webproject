bdc6c9246e9f1abdec335df090ded3ab1351d031
Sebastian <sebastian@email.com>
2024-05-14 16:33:09 +0200

    Small change

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let BASE_URL;
 
 if (process.env.NODE_ENV === "production") {
-  BASE_URL = "http://localhost:8094";
+  BASE_URL = "http://backend:8094";
 } else {
   BASE_URL = "http://localhost:8085";
 }
