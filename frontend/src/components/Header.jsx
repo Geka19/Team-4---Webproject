c0ba13cd45493815efae43dcc2183a507dd550c5
simen <111754546+simeel@users.noreply.github.com>
2024-05-21 11:19:33 +0200

    page header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="page-header">
+      <img src="/svg/boardable-logo-text.svg" alt="" srcset="" />
+    </header>
+  );
+};
+
+export default Header;
